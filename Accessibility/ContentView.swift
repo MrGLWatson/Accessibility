777b2402bb3b91aed8f20c1b726ea28061c74a36
Gary Watson <Gary_Watson@Blueyonder.co.uk>
2020-07-23 09:39:17 +0100

    Initial Commit

---
+++ Accessibility/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Accessibility
+//
+//  Created by Gary Watson on 23/07/2020.
+//  Copyright © 2020 Gary Watson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
